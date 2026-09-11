a064f8d88d59d1db50c46eb86570a4732bf1bc28
Sam <thesamloop@gmail.com>
2026-09-11 01:33:45 -0400

    Add date posted to message card

--- App.tsx
+++ App.tsx
@@ -70,7 +70,9 @@ export default function App() {
 
   const formatTime = (iso: string) => {
     const d = new Date(iso);
-    return d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const date = `${String(d.getMonth() + 1).padStart(2, '0')}/${String(d.getDate()).padStart(2, '0')}/${d.getFullYear()}`;
+    const time = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return `${date} ${time}`;
   };
 
   // Render a single post
